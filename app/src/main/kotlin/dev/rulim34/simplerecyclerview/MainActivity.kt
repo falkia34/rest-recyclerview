28dbb06dab80d591ca5fd5f3e9470d6e34caac71
Muhammad Naufal <me@falkia34.dev>
2024-11-09 21:45:23 +0700

    fix: Set actionbar toolbar

--- app/src/main/kotlin/dev/rulim34/simplerecyclerview/MainActivity.kt
+++ app/src/main/kotlin/dev/rulim34/simplerecyclerview/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
